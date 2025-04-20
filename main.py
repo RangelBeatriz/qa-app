2d7c609ec4daa88205d62d2534dec0ddcbe4569c
RangelBeatriz <rangelbeatriz@id.uff.br>
2025-04-20 20:05:23 -0300

    Adding function to upload and the pdf content.

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
+from pdf_utils import extract_text_from_pdf
 
 app = FastAPI()
+
+UPLOAD_DIR = "models"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -11,3 +16,12 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def get_home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload/")
+async def upload_pdf(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    
+    text = extract_text_from_pdf(file_path)
+    return {"filename": file.filename, "text_excerpt": text[:300]}
