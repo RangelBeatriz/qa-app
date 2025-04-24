a12d56607d5a61e134c135eb602de35076c9f57e
RangelBeatriz <rangelbeatriz@id.uff.br>
2025-04-24 19:53:47 -0300

    Building model training with LoRA

--- train_flan_lora.py
+++ train_flan_lora.py
@@ -3,7 +3,10 @@ from datasets import load_dataset
 from peft import get_peft_model, LoraConfig, TaskType
 import torch
 
-
+#Loading base model.
 model_name = "google/flan-t5-base"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+#Configuring LoRA
+lora_config = LoraConfig(r=8, lora_alpha=16, target_modules=["q", "v"], lora_dropout=0.1, bias="none", task_type=TaskType.SEQ_2_SEQ_LM)
