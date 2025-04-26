c506dfd2a2b67f05ef06d410fd0c4ae158cceb89
RangelBeatriz <rangelbeatriz@id.uff.br>
2025-04-25 23:35:15 -0300

    Tokenizing the data

--- train_flan_lora.py
+++ train_flan_lora.py
@@ -10,3 +10,16 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 #Configuring LoRA
 lora_config = LoraConfig(r=8, lora_alpha=16, target_modules=["q", "v"], lora_dropout=0.1, bias="none", task_type=TaskType.SEQ_2_SEQ_LM)
+
+model = get_peft_model(model, lora_config)
+
+#tokenizing the data
+dataset = load_dataset("json", data_files="data/editais_data.jsonl")["train"]
+
+def tokenize(sample):
+    inputs = tokenizer(sample["instruction"], padding="max_length", truncation=True, max_length=512)
+    targets = tokenizer(sample["output"], padding="max_length", truncation=True, max_length=128)
+    inputs["labels"] = targets["input_ids"]
+    return inputs
+
+tokenized_dataset = dataset.map(tokenize)
